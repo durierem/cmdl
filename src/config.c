9c00fc14f18b1d5d6da86d2508487e207ff5c14c
Rémi Durieu <remi.durieu@univ-rouen.fr>
2020-12-22 17:13:11 +0100

    Corrige oubli de config.c

---
+++ src/config.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "config.h"
+
+enum __OPTION {
+    DAEMON_WORKER_MAX,
+    REQUEST_QUEUE_MAX
+};
+
+static const char *optflags[] = {
+    "DAEMON_WORKER_MAX",
+    "REQUEST_QUEUE_MAX"
+};
+
+#define LINE_LENGTH_MAX 128
+#define SEPARATOR '\t'
+#define COMMENT '#'
+
+int __load(enum __OPTION opt, const char *filename) {
+    FILE *f = fopen(filename, "r");
+    if (f == NULL) {
+        return -1;
+    }
+    
+    int ret = -1;
+
+    char line[LINE_LENGTH_MAX] = { 0 };
+    while (fgets(line, sizeof(line), f) != NULL) {
+        if (*line == COMMENT || *line == '\n') {
+            continue;
+        }
+        char flagbuf[LINE_LENGTH_MAX] = { 0 };
+        int i = 0;
+        while (line[i] != SEPARATOR) {
+            flagbuf[i] = line[i];
+            i++;
+        }
+
+        const char *flag = optflags[opt];
+        if (strncmp(flag, flagbuf, strlen(flag)) == 0) {
+            while (line[i] == SEPARATOR) {
+                i++;
+            }
+
+            int j = 0;
+            char retbuf[16];
+            while (line[i] != '\0') {
+                retbuf[j] = line[i];
+                i++;
+                j++;
+            }
+
+            ret = (int) strtol(retbuf, NULL, 10);
+            break;
+        }
+    }
+
+    fclose(f);
+    return ret;
+}
+
+
+int config_load(struct config *ptr, const char *filename) {
+    int ret = __load(DAEMON_WORKER_MAX, filename);
+    if (ret == -1) {
+        return -1;
+    }
+    ptr->DAEMON_WORKER_MAX = ret;
+
+    ret = __load(REQUEST_QUEUE_MAX, filename);
+    if (ret == -1) {
+        return -1;
+    }
+    ptr->REQUEST_QUEUE_MAX = ret;
+
+    return 0;
+}
